f422e919cef70d9557b894cdb35e69f48590d336
Azeb Tesfay <azbseb@gmail.com>
2026-05-16 07:30:43 -0700

    add useeffect in videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,5 +1,16 @@
 
+import { useEffect } from "react"
+import { getVideos } from "../mockApi.js"
+//import { data } from "react-router-dom"
+
+
+
 export default function Videos() {
+ useEffect(() => {
+  getVideos().then((data) => {
+   console.log("getVideos returned:", data)
+  }, [])
+ })
  return (
   <div>
    <h1>Available Videos</h1>
