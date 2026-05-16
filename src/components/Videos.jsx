6490c1ae4d730d0c2ba0bd8a42e69e647adfb846
Ebtisam Jelani <93232997+Ebtisam0402@users.noreply.github.com>
2026-05-16 08:02:48 -0700

    rendered the list

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,20 +1,27 @@
 
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getVideos } from "../mockApi.js"
 //import { data } from "react-router-dom"
 
 
 
 export default function Videos() {
+    const [videos, setVideos] = useState([]);
  useEffect(() => {
-  getVideos().then((data) => {
-   console.log("getVideos returned:", data)
-  })
+  getVideos().then((data) => setVideos(data))
+   //console.log("getVideos returned:", data)
+  
  }, [])
  return (
   <div>
    <h1>Available Videos</h1>
-   <p>Video list will go here.</p>
+     <ul>
+        {videos.map((filename) => (
+          <li key={filename}>{filename}</li>
+        ))}
+      </ul>
+
+   
   </div>
  )
 }
